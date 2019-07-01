925f2bfdb63ddf7a822b60805bd393a8700951f5
Joachim Strömbergson <joachim@assured.se>
2019-07-01 14:33:55 +0200

    Adding AES round functions mixcolumns and addroundkey. Now we just need a number of sboxes instantiated.

--- src/rtl/aes_mask.v
+++ src/rtl/aes_mask.v
@@ -74,6 +74,63 @@ module aes_mask(
   // Instantiations.
   //----------------------------------------------------------------
 
+  //----------------------------------------------------------------
+  // AES round functions with sub functions.
+  //----------------------------------------------------------------
+  function [7 : 0] gm2(input [7 : 0] op);
+    begin
+      gm2 = {op[6 : 0], 1'b0} ^ (8'h1b & {8{op[7]}});
+    end
+  endfunction // gm2
+
+  function [7 : 0] gm3(input [7 : 0] op);
+    begin
+      gm3 = gm2(op) ^ op;
+    end
+  endfunction // gm3
+
+  function [31 : 0] mixw(input [31 : 0] w);
+    reg [7 : 0] b0, b1, b2, b3;
+    reg [7 : 0] mb0, mb1, mb2, mb3;
+    begin
+      b0 = w[31 : 24];
+      b1 = w[23 : 16];
+      b2 = w[15 : 08];
+      b3 = w[07 : 00];
+
+      mb0 = gm2(b0) ^ gm3(b1) ^ b2      ^ b3;
+      mb1 = b0      ^ gm2(b1) ^ gm3(b2) ^ b3;
+      mb2 = b0      ^ b1      ^ gm2(b2) ^ gm3(b3);
+      mb3 = gm3(b0) ^ b1      ^ b2      ^ gm2(b3);
+
+      mixw = {mb0, mb1, mb2, mb3};
+    end
+  endfunction // mixw
+
+  function [127 : 0] mixcolumns(input [127 : 0] data);
+    reg [31 : 0] w0, w1, w2, w3;
+    reg [31 : 0] ws0, ws1, ws2, ws3;
+    begin
+      w0 = data[127 : 096];
+      w1 = data[095 : 064];
+      w2 = data[063 : 032];
+      w3 = data[031 : 000];
+
+      ws0 = mixw(w0);
+      ws1 = mixw(w1);
+      ws2 = mixw(w2);
+      ws3 = mixw(w3);
+
+      mixcolumns = {ws0, ws1, ws2, ws3};
+    end
+  endfunction // mixcolumns
+
+  function [127 : 0] addroundkey(input [127 : 0] data, input [127 : 0] rkey);
+    begin
+      addroundkey = data ^ rkey;
+    end
+  endfunction // addroundkey
+
 
   //----------------------------------------------------------------
   // Concurrent connectivity for ports etc.
@@ -113,8 +170,14 @@ module aes_mask(
   //----------------------------------------------------------------
   always @*
     begin : round_logic
+      reg [127 : 0] mixed_state;
+      reg [127 : 0] rkey_state;
+
       state_new = 128'h0;
       state_we  = 1'h0;
+
+      mixed_state = mixcolumns(state_reg);
+      rkey_state = addroundkey(mixed_state, round_key_reg);
 
       if (init)
         begin
@@ -126,15 +189,7 @@ module aes_mask(
       if (next)
         begin
           state_we = 1'h1;
-
-          // MixColumns
-
-
-          // SubBytes
-
-
-          // AddRoundKey
-          state_new = state_reg ^ round_key_reg;
+          state_new = rkey_state;
         end
 
 
